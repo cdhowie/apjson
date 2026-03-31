8779795b997ddcaeaf15ef633f91da86977a3fbc
Chris Howie <me@chrishowie.com>
2026-03-31 17:46:15 -0400

    Allow skipping specific benchmark categories

--- benchmarks/benchtemplate.py
+++ benchmarks/benchtemplate.py
@@ -1,9 +1,16 @@
-import pyperf
-
 import pathlib
+import pyperf
+import os
 
 def run_bench(load, dump):
     runner = pyperf.Runner()
+
+    runner.parse_args()
+
+    if runner.args.inherit_environ is None:
+        runner.args.inherit_environ = []
+
+    runner.args.inherit_environ.extend(['BENCH_SKIP_LOAD', 'BENCH_SKIP_DUMP'])
 
     for entry in pathlib.Path('testfiles').iterdir():
         if entry.is_file():
@@ -12,5 +19,7 @@ def run_bench(load, dump):
 
             decoded = load(encoded)
 
-            runner.bench_func(f"{entry.name} load", load, encoded)
-            runner.bench_func(f"{entry.name} dump", dump, decoded)
+            if 'BENCH_SKIP_LOAD' not in os.environ:
+                runner.bench_func(f"{entry.name} load", load, encoded)
+            if 'BENCH_SKIP_DUMP' not in os.environ:
+                runner.bench_func(f"{entry.name} dump", dump, decoded)
